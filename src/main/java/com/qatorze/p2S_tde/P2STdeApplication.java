eb080baa183b7dbf02c567672c2165aa5e3fd5ad
Cédric Vianney Mokoko Bayito <cedricbayito@gmail.com>
2024-12-01 07:24:17 +0100

    first commit

---
+++ src/main/java/com/qatorze/p2S_tde/P2STdeApplication.java
@@ -0,0 +1,13 @@
+package com.qatorze.p2S_tde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2STdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2STdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qatorze/p2S_tde/P2STdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qatorze.p2S_tde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2STdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
